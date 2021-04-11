3377c11141c48016c9b7e593244657886d259284
stetsiuks <82323085+stetsiuks@users.noreply.github.com>
2021-04-11 18:21:00 +0300

    Update Program.cs

--- TCPP-Stetsiuk-Project/Program.cs
+++ TCPP-Stetsiuk-Project/Program.cs
@@ -6,8 +6,10 @@ namespace TCPP_Stetsiuk_Project
     {
         static void Main(string[] args)
         {
+            //Змінено в браузері
             
             Console.WriteLine("Hello World from ALPHA2_commit!");
+             Console.WriteLine("Hello World from ALPHA_changed in browser!");
         }
     }
 }
