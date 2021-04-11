57a46dc633fdcd83f3dcec24e42ac3d4fb6a5e13
Сергій Стецюк <stetsiuk.s@donnu.edu.ua>
2021-04-11 17:37:25 +0300

    Добавьте файлы проекта.

---
+++ TCPP-Stetsiuk-Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPP_Stetsiuk_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
